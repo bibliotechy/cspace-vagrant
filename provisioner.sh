f494d8158b3ccb408cae4bd331065cd0ee8242f9
Chad Nelson <chadbnelson@gmail.com>
2015-01-13 13:29:05 -0500

    Add startup and authorities init to provision script

--- provisioner.sh
+++ provisioner.sh
@@ -3,5 +3,10 @@
 wget https://raw.github.com/cspace-puppet/cspace_puppet_bootstrap/master/scripts/bootstrap-cspace-modules.sh
 chmod u+x bootstrap-cspace-modules.sh
 sudo ./bootstrap-cspace-modules.sh -y
+sudo su cspace
+$CSPACE_JEESERVER_HOME/bin/startup.sh
+wget https://raw.githubusercontent.com/collectionspace/Tools/master/scripts/init-authorities.sh
+chmod u+x init-authorities.sh
+sudo ./init-authorities.sh
 
 
